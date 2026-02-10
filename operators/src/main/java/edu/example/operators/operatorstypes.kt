2e355e04f7041378b6a0bcce3ae4f0c029871b7a
ipajaro11 <ipajaro11@alumnos.uaq.mx>
2026-02-10 09:35:48 -0600

    Es un comit de la parte de boxing

--- operators/src/main/java/edu/example/operators/operatorstypes.kt
+++ operators/src/main/java/edu/example/operators/operatorstypes.kt
@@ -10,4 +10,28 @@ fun main (){
     println(6*50)
     println(6.0*50.0)
     println(6.0*50)
+
+    println(2.times(3))
+    println(3.5.plus(4))
+    println(2.4.div(2))
+
+    val i: Int =6
+    val b1 = i.toByte()
+    println(b1)
+    val b2: Byte = 1
+    println(b2)
+
+    //val i1: Int = b2
+    //val i2: String =b2
+    //val i3: Double = b2
+    //Estas tres lineas de codigo arriba escritas tienen un error porque se desea imprmirlas en un cierto tipo de variable cuando el original es de tipo Byte
+
+    //para evitar esos errores se hace un casting de la siguiente manera
+
+    val i4: Int = b2.toInt()
+    println(i4)
+    val i5: String = b2.toString()
+    println(i5)
+    val i6: Double = b2.toDouble()
+    println(i6)
 }
