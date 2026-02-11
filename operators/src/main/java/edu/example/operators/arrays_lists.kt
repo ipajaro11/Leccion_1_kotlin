e9691bc97ea986c03d7f8a59a271a3ffcffd387e
ipajaro11 <ipajaro11@alumnos.uaq.mx>
2026-02-11 08:56:31 -0600

    Comit modulo 1

---
+++ operators/src/main/java/edu/example/operators/arrays_lists.kt
@@ -0,0 +1,12 @@
+package edu.example.operators
+
+fun main (){
+    //haceindo listas
+    val school = listOf("mackerel", "trout", "halibut")
+    println(school)
+
+    val mylist = mutableListOf("tuna", "salmon", "shark")
+    mylist.remove("shark")
+
+
+}
