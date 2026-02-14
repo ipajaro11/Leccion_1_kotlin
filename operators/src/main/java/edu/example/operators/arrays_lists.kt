e41876b6e9bc891eee765284b8e819ba3a9fea26
ipajaro11 <ipajaro11@alumnos.uaq.mx>
2026-02-14 00:37:55 -0600

    Comit modulo 1

--- operators/src/main/java/edu/example/operators/arrays_lists.kt
+++ operators/src/main/java/edu/example/operators/arrays_lists.kt
@@ -9,4 +9,59 @@ fun main (){
     mylist.remove("shark")
     println(mylist)
 
+    val school1 = arrayOf("shark", "salmon", "minnow")
+    println(java.util.Arrays.toString(school1))
+
+    val mix = arrayOf("fish", 2)  //Aqui no se imprime los valores como dice en el curso de kotlin
+    println(mix)
+
+    val numbers = intArrayOf(1,2,3)
+    println(numbers)
+
+    val numbers3 = intArrayOf(4,5,6)
+    val foo2 = numbers3 + numbers
+    println(foo2[5])
+
+    val oceans = listOf("Atlantic", "Pacific")
+    val oddList = listOf(numbers, oceans, "salmon")
+    println(oddList)
+
+    val array = Array (5) { it * 2 }
+    println(java.util.Arrays.toString(array))
+
+
+    //Creando loops
+
+    val school2 = arrayOf("shark", "salmon", "minnow")
+    for (element in school2) {
+        print(element + " ")
+    }
+
+    for ((index, element) in school2.withIndex()) {
+        println("Item at $index is $element\n")
+    }
+
+for (i in 1..5) print(i)
+
+    for (i in 5 downTo 1) print(i)
+
+    for (i in 3..6 step 2) print(i)
+
+    for (i in 'd'..'g') print (i)
+
+    var bubbles = 0
+    while (bubbles < 50) {
+        bubbles++
+    }
+    println("$bubbles bubbles in the water\n")
+
+    do {
+        bubbles--
+    } while (bubbles > 50)
+    println("$bubbles bubbles in the water\n")
+
+    repeat(2) {
+        println("A fish is swimming")
+    }
+
 }
