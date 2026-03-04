c16bed1d26cc02d9e181e03d260bb48f89f04aa0
ipajaro11 <ipajaro11@alumnos.uaq.mx>
2026-03-04 09:00:05 -0600

    Comit ejercicio maquina de dulces

--- CandyMachine/src/main/java/edu/example/candymachine/Dispenser.kt
+++ CandyMachine/src/main/java/edu/example/candymachine/Dispenser.kt
@@ -1,5 +1,17 @@
 package edu.example.candymachine
 
 class Dispenser {
+    private var numberOfItems: Int
+    private var cost: Int
 
+    constructor(){
+        numberOfItems = 50
+        cost = 50
+    }
+
+    constructor(setNoOfItems: Int, setCost: Int){
+        if(setNoOfItems >= 0){
+
+        }
+    }
 }
