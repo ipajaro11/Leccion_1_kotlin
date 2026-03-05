c8d2ab71c2afbffc387f5a363825c114a1872f6c
ipajaro11 <ipajaro11@alumnos.uaq.mx>
2026-03-04 22:59:15 -0600

    Comit ejercicio maquina de dulces

--- CandyMachine/src/main/java/edu/example/candymachine/Dispenser.kt
+++ CandyMachine/src/main/java/edu/example/candymachine/Dispenser.kt
@@ -10,8 +10,26 @@ class Dispenser {
     }
 
     constructor(setNoOfItems: Int, setCost: Int){
-        if(setNoOfItems >= 0){
+        if(setNoOfItems >= 0)
+            numberOfItems = setNoOfItems
+        else
+            numberOfItems = 50
 
-        }
+        if(setCost>=0)
+            cost=setCost
+        else
+            cost = 50
+    }
+
+    fun getCount (): Int{
+        return numberOfItems
+    }
+
+    fun getProductCost(): Int{
+        return cost
+    }
+
+    fun makeSale (){
+        numberOfItems--
     }
 }
--- CandyMachine/src/main/java/edu/example/candymachine/Principal.kt
+++ CandyMachine/src/main/java/edu/example/candymachine/Principal.kt
@@ -1,12 +1,64 @@
 package edu.example.candymachine
+import java.util.Scanner
 
-fun candyMachine()
-{
-    val myCandyMachine = CashRegister()
-    myCandyMachine.currentBalance()
-    myCandyMachine.acceptAmount(amountIn = 200)
+fun showSelection(){
+    println("***Welcome to Shelly's Candy Shoop***")
+    println("To select an item, enter")
+    println("1 for Candy")
+    println("2 for Chips")
+    println("3 for Gum")
+    println("4 for Cookies")
+    println("9 to exit")
 }
 
-fun main(){
-    candyMachine()
+fun sellProduct (product: Dispenser, cRegister: CashRegister){
+    var price: Int
+    var coinsInserted: Int
+    var coinsRequired: Int
+
+    if(product.getCount() > 0){
+        price = product.getProductCost()
+        coinsRequired = price
+        coinsInserted = 0
+
+        while (coinsRequired > 0)
+        {
+            val console = Scanner(System.`in`)
+            print("Please deposit " + coinsRequired + " cents: ")
+            coinsInserted = coinsInserted + console.nextInt()
+            coinsRequired = price-coinsInserted
+        }
+        println()
+        cRegister.acceptAmount(coinsInserted)
+        product.makeSale()
+        println("Collect your item at the bottom and enjoy.\n")
+    }
+    else{
+        println("Sorry this item is sold out.\n")
+    }
+}
+
+fun main (){
+    var cashRegister = CashRegister()
+    var candy = Dispenser (100,50)
+    var chips = Dispenser (100,65)
+    var gums = Dispenser (75,45)
+    var cookies = Dispenser (100,85)
+    val console = Scanner(System.`in`)
+    var choice: Int
+
+    showSelection()
+    choice = console.nextInt()
+
+    while (choice != 9){
+        when (choice){
+            1 -> sellProduct(candy, cashRegister)
+            2 -> sellProduct(chips,cashRegister)
+            3 -> sellProduct(gums, cashRegister)
+            4 -> sellProduct(cookies, cashRegister)
+            else -> println("Invalid selection")
+        }
+        showSelection()
+        choice = console.nextInt()
+    }
 }
